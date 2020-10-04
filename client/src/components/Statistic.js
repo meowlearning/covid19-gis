081795c17770def27a41252a4f3baa68a9e8cfec
Jerry Nguyen <n10327622@qut.edu.au>
2020-10-04 11:34:56 +1000

    Frontend: fix conflict colors (active vs recovered)

--- client/src/components/Statistic.js
+++ client/src/components/Statistic.js
@@ -27,36 +27,38 @@ class StatisticSummary extends Component {
                     graph: [{
                         "case": "Confirmed",
                         "Confirmed": this.props.data.confirmed,
-                        "ConfirmedColor": "hsl(0, 0%, 0%)"
+                        "color": "#8DD3C7"
                     }, {
                         "case": "Active",
                         "Active": this.props.data.active,
-                        "ActiveColor": "hsl(92, 100%, 26%)"
+                        "color": "#FB8072"
                     }, {
                         "case": "Recovered",
                         "Recovered": this.props.data.recovered,
-                        "RecoveredColor": "hsl(92, 100%, 26%)"
+                        "color": "#BEBADA"
                     }, {
                         "case": "Deaths",
                         "Deaths": this.props.data.deaths,
-                        "DeathsColor": "hsl(355, 83%, 44%)"
-                    }]
+                        "color": "#FFE92B"
+                    }],
                 }
             })
         }
     }
 
     render() {
+        const data = this.state.data.graph;
+
         return (
             <Card title="World Statistic" extra={<Tooltip info={this.state.info} />}>
 
                 <div className="graph-summary" style={{ width: "100%", height: "36.3vh" }}>
-                    {(this.state.data.graph !== null) ?
+                    {(data !== null) ?
                         <ResponsiveBar
-                            data={this.state.data.graph}
-                            keys={["Confirmed", "Deaths", "Recovered", "Active"]}
+                            data={data}
+                            keys={data.map(c => c.case)}
                             indexBy="case"
-                            colors={{ scheme: 'set3' }}
+                            colors={data.map(c => c.color)}
                             labelTextColor="none"
                             animate={true}
                             motionStiffness={115}
@@ -69,22 +71,22 @@ class StatisticSummary extends Component {
                 <br />
                 <br />
                 <div className="Statistic-summary" >
-                    {(this.props.data != null) ?
+                    {(this.props.data != null && data != null) ?
                         <div className="Container">
                             <Row gutter={[48, 48]}>
                                 <Col key="Confirmed" span={12}>
-                                    <Statistic title="Confirmed" value={this.props.data.confirmed} valueStyle={{ color: "#8DD3C7" }} />
+                                    <Statistic title="Confirmed" value={this.props.data.confirmed} valueStyle={{ color: data.find(c => c.case == "Confirmed").color }} />
                                 </Col>
                                 <Col key="Deaths" span={12}>
-                                    <Statistic title="Deaths" value={this.props.data.deaths} valueStyle={{ color: '#FDF5B3' }} />
+                                    <Statistic title="Deaths" value={this.props.data.deaths} valueStyle={{ color: data.find(c => c.case == "Deaths").color }} />
                                 </Col>
                             </Row>
                             <Row gutter={[48, 48]}>
                                 <Col key="Recovered" span={12}>
-                                    <Statistic title="Recovered" value={this.props.data.recovered} valueStyle={{ color: '#BEBADA' }} />
+                                    <Statistic title="Recovered" value={this.props.data.recovered} valueStyle={{ color: data.find(c => c.case == "Recovered").color }} />
                                 </Col>
                                 <Col key="Active" span={12}>
-                                    <Statistic title="Active" value={this.props.data.active} valueStyle={{ color: '#BEBADA' }} />
+                                    <Statistic title="Active" value={this.props.data.active} valueStyle={{ color: data.find(c => c.case == "Active").color }} />
                                 </Col>
                             </Row>
                         </div>
